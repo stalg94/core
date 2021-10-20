06446975858c6b70fec0ac1bbdf651a1430d19fe
stalg94 <stalg28@gmail.com>
2021-10-20 21:33:15 +0300

    Update/insert
    insert commented

---
+++ BookStoresWebAPI/BookStoresWebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+﻿using BookStoresWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace BookStoresWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+  
+        [HttpGet]
+        public IEnumerable<Models.Publisher> Get()
+        {
+            //CRUD
+            using (var _context = new BookStoresDBContext())
+            {
+                //Models.Publisher publisher = new Models.Publisher();
+                //publisher.PublisherName = "Egmont Book";
+
+                //_context.Publishers.Add(publisher);
+
+                //_context.SaveChanges();
+                Models.Publisher publisher = _context.Publishers.FirstOrDefault();
+                publisher.PublisherName = "Egmond Books";
+
+                _context.SaveChanges();
+
+
+                return _context.Publishers.ToList();
+            }
+
+
+
+
+        }
+    }
+}
---
+++ BookStoresWebAPI/BookStoresWebAPI/Models/BookStoresDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoresWebAPI.Models
+{
+    public class BookStoresDBContext :DbContext
+    {
+        public DbSet<Publisher> Publishers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(@"Data Source=localhost;Initial Catalog=BookStoresDB;Integrated Security=True");
+
+        }
+    }
+}
